17c0b5e8720c7a954ad17307fd465020d3535e1d
Matheus Navarro <matheusnavarro@alu.ufc.br>
2020-10-02 22:38:09 -0300

    feat(back): adiciona filtro de autenticacao do jwt
    re #28

--- backend/src/main/java/com/api/caramelo/CarameloApplication.java
+++ backend/src/main/java/com/api/caramelo/CarameloApplication.java
@@ -1,13 +1,25 @@
 package com.api.caramelo;
 
+import com.api.caramelo.filters.AuthFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CarameloApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarameloApplication.class, args);
+	}
+
+	@Bean
+	public FilterRegistrationBean<AuthFilter> filterFilterRegistrationBean() {
+		FilterRegistrationBean<AuthFilter> registrationBean = new FilterRegistrationBean<>();
+		AuthFilter authFilter = new AuthFilter();
+		registrationBean.setFilter(authFilter);
+		registrationBean.addUrlPatterns("/pets/*");
+		return registrationBean;
 	}
 
 }
